e6be5395704b45f700d54cd19bfeb83f36472879
Ryan Melton <ryan@openc3.com>
2023-04-11 08:30:43 -0600

    move source transactions to cfdp user

--- microservices/CFDP/app/controllers/cfdp_controller.rb
+++ microservices/CFDP/app/controllers/cfdp_controller.rb
@@ -24,26 +24,7 @@ class CfdpController < ApplicationController
       render :json => { :status => 'error', :message => "destination_entity_id must be numeric" }, :status => 400
       return
     end
-
-    transaction = CfdpSourceTransaction.new
-    Thread.new do
-      begin
-        transaction.put(
-          destination_entity_id: params[:destination_entity_id],
-          source_file_name: params[:source_file_name],
-          destination_file_name: params[:destination_file_name],
-          transmission_mode: params[:transmission_mode],
-          closure_requested: params[:closure_requested],
-          filestore_requests: params[:filestore_requests],
-          fault_handler_overrides: params[:fault_handler_overrides],
-          messages_to_user: params[:messages_to_user],
-          flow_label: params[:flow_label],
-          segmentation_contro: params[:segmentation_control]
-        )
-      rescue => err
-        OpenC3::Logger.error(err.formatted, scope: ENV['OPENC3_SCOPE'])
-      end
-    end
+    transaction = $cfdp_user.start_source_transaction(params)
     render json: transaction.id
   rescue ActionController::ParameterMissing => error
     render :json => { :status => 'error', :message => error.message }, :status => 400
