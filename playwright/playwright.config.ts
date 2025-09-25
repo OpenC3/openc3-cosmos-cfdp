b4b48319d0fce9c50f8c8a5c649b36563e90964e
Jason Thomas <jason@openc3.com>
2025-09-25 07:48:40 -0600

    Enable playwright trace

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:2900',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: process.env.CI ? 'on-first-retry' : 'on',
+    trace: 'on', //process.env.CI ? 'on-first-retry' : 'on',
     screenshot: 'only-on-failure',
   },
 
