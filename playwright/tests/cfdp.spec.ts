b976e3970c419d65dcd30f04e1eb360aecb5a55d
Jason Thomas <jason@openc3.com>
2026-03-13 09:55:27 -0600

    Fix playwright

--- playwright/tests/cfdp.spec.ts
+++ playwright/tests/cfdp.spec.ts
@@ -25,7 +25,7 @@ test.describe.configure({ mode: 'serial' })
 test.setTimeout(1800000) // 30min
 
 const plugin = 'openc3-cosmos-cfdp'
-const pluginGem = 'openc3-cosmos-cfdp-1.1.0.gem'
+const pluginGem = 'openc3-cosmos-cfdp-0.0.0.gem'
 
 test('installs the CFDP plugin', async ({ page, utils }) => {
   await page.goto('/tools/admin/plugins')
@@ -47,8 +47,8 @@ test('installs the CFDP plugin', async ({ page, utils }) => {
   ])
   await fileChooser.setFiles(`../${pluginGem}`)
   await expect(page.locator('.v-dialog:has-text("Variables")')).toBeVisible()
-  await page.getByLabel('plugin_test_mode', { exact: true }).dblclick()
-  await page.getByLabel('plugin_test_mode', { exact: true }).fill('true')
+  await page.getByRole('combobox', { name: 'plugin_test_mode' }).click()
+  await page.getByRole('option', { name: 'TRUE' }).click()
   await page.locator('data-test=edit-submit').click()
   await expect(page.locator('[data-test=plugin-alert]')).toContainText(
     'Started installing',
@@ -184,17 +184,17 @@ test('continues transaction after microservice restart', async ({
     .locator('.v-list-item')
     .filter({ hasText: 'DEFAULT__USER__CFDP' })
     .first()
-    .locator('.mdi-play')
+    .getByRole('button', { name: 'Restart Microservice' })
     .click()
-  await pageTwo.locator('[data-test="confirm-dialog-start"]').click()
+  await pageTwo.locator('[data-test="confirm-dialog-restart"]').click()
   await utils.sleep(1000)
   await pageTwo
     .locator('.v-list-item')
     .filter({ hasText: 'DEFAULT__USER__CFDP2' })
     .first()
-    .locator('.mdi-play')
+    .getByRole('button', { name: 'Restart Microservice' })
     .click()
-  await pageTwo.locator('[data-test="confirm-dialog-start"]').click()
+  await pageTwo.locator('[data-test="confirm-dialog-restart"]').click()
 
   await expect(page.locator('[data-test=output-messages]')).toContainText(
     'Script completed: CFDP/procedures/interrupt_test.rb',
